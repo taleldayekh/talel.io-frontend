4c63f6fee6f218ce1123d1bcea229ee6d3622906
taleldayekh <mail@talel.se>
2021-01-04 20:02:43 +0100

    Creating CD pipeline for deployment to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,19 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App: React.FC = () => {
-  const [welcomeMessage, setWelcomeMessage] = useState<string>('')
+  const [welcomeMessage, setWelcomeMessage] = useState<string>('');
 
   useEffect(() => {
-    if (welcomeMessage) return
+    if (welcomeMessage) return;
 
-    axios.get('http://localhost:5000/api/v1/user').then(res => setWelcomeMessage(res.data.message))
-  })
+    axios
+      .get('http://54.173.176.222:5000/api/v1/user/')
+      .then((res) => setWelcomeMessage(res.data.message));
+  });
 
-  return <>
-  {welcomeMessage ? (
-    <h1>{welcomeMessage}</h1>
-  ) : (
-    <h1>Loading message</h1>
-  )}
-  </>
-}
+  return (
+    <>{welcomeMessage ? <h1>{welcomeMessage}</h1> : <h1>Loading message</h1>}</>
+  );
+};
 
 export default App;
