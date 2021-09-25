21479fcc2afb7ee864e3258486ecd71d3d974145
taleldayekh <dev@talel.io>
2021-09-25 23:13:03 +0200

    Temporarily disable tests for CD pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import ProtectedView from 'src/views/private/protectedView/ProtectedView';
 import HomeView from 'src/views/homeView/HomeView';
 import LoginFormView from 'src/views/loginFormView/LoginFormView';
