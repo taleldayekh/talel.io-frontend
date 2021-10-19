03dbb882901b8f062a07b0a823d5b393b91095dc
taleldayekh <dev@talel.io>
2021-10-19 21:32:41 +0200

    Adding mock data

---
+++ src/tests/mock-data/auth.ts
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import { USERNAME_TALEL, INITIAL_USER_ID } from 'src/tests/constants';
+
+const ACCESS_TOKEN = jwt.sign(
+  {
+    user_id: INITIAL_USER_ID,
+    username: USERNAME_TALEL,
+    exp: new Date().getTime(),
+  },
+  '',
+);
+
+const EXPIRED_ACCESS_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VybmFtZSI6InRhbGVsZGF5ZWtoIiwiZXhwIjo1MTgzMDY0MDB9.hpMuz--iQLSKOW1pkJfhs6mbl2wJo1rkwPfNWoWh-A8';
+
+export { ACCESS_TOKEN, EXPIRED_ACCESS_TOKEN };
---
+++ src/tests/mock-data/users.ts
@@ -0,0 +1,8 @@
+import { EMAIL_TALEL, PASSWORD } from 'src/tests/constants';
+
+const talelLoginData = {
+  email: EMAIL_TALEL,
+  password: PASSWORD,
+};
+
+export { talelLoginData };
