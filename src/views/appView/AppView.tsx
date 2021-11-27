718bb0aed0cd35a03d4fb97f16ecfc252dbd2ba6
taleldayekh <dev@talel.io>
2021-11-27 13:27:16 +0100

    Updating React router and routes

--- src/views/appView/AppView.tsx
+++ src/views/appView/AppView.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import ProtectedView from 'src/views/private/protectedView/ProtectedView';
-import LoginFormViewController from 'src/views/loginFormView/LoginFormViewController';
-import HomeView from 'src/views/homeView/HomeView';
-import AdminView from 'src/views/private/adminView/AdminView';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import ProtectedViewsController from 'src/views/private/ProtectedViewsController/ProtectedViewsController';
+import LoginController from 'src/views/LoginView/LoginController';
 
 const AppView: React.FC = () => {
   return (
-    <Router>
-      <Route exact path="/" component={HomeView} />
-      <Route path="/login" component={LoginFormViewController} />
-      <ProtectedView path="/admin" component={AdminView} />
-    </Router>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<p>This is the landing page</p>} />
+        <Route path="/login" element={<LoginController />} />
+        <Route
+          path="/admin/*"
+          element={
+            <ProtectedViewsController>
+              <p>If you made it this far you are logged in</p>
+            </ProtectedViewsController>
+          }
+        />
+      </Routes>
+    </HashRouter>
   );
 };
 
