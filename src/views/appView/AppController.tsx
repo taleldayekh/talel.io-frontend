1cd4965fbdd10f345e4daf478985f3a459496521
taleldayekh <dev@talel.io>
2022-01-09 15:16:53 +0100

    Adding AppController with auth token interceptor

---
+++ src/views/appView/AppController.tsx
@@ -0,0 +1,46 @@
+import React, { useContext, useEffect } from 'react';
+import axios, { AxiosRequestConfig } from 'axios';
+import { AuthenticationContext } from 'src/contexts/authentication/authentication.context';
+import useAuthentication from 'src/hooks/authentication/useAuthentication';
+import AppView from 'src/views/AppView/AppView';
+
+const AppController: React.FC = () => {
+  const { authenticationContext } = useContext(AuthenticationContext);
+  const authentication = useAuthentication();
+
+  const accessToken = authenticationContext.accessToken;
+
+  const interceptorId = axios.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const requestUrl = config.url ? config.url : '';
+
+      if (!accessToken) return config;
+      if (
+        config.method === 'post' &&
+        !authentication.requestRequiresAuthentication(requestUrl)
+      )
+        return config;
+
+      const accessTokenHasExpired: boolean =
+        authentication.accessTokenHasExpired(accessToken);
+
+      if (accessTokenHasExpired) {
+        authentication.refreshAccessToken();
+      }
+
+      config.headers.Authorization = `Bearer ${accessToken}`;
+
+      return config;
+    },
+  );
+
+  useEffect(() => {
+    return () => {
+      axios.interceptors.request.eject(interceptorId);
+    };
+  });
+
+  return <AppView />;
+};
+
+export default AppController;
