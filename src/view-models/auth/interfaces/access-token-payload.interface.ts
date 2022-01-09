211728e424b911851a94c200179ab42fb49c627f
taleldayekh <dev@talel.io>
2022-01-09 15:29:19 +0100

    Deleting obsolete auth code

--- src/view-models/auth/interfaces/access-token-payload.interface.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable camelcase */
-
-export interface AccessTokenPayload {
-  user_id: number;
-  username: string;
-  exp: number;
-}
--- src/view-models/auth/interfaces/index.ts
+++
@@ -1,2 +0,0 @@
-export * from 'src/view-models/auth/interfaces/access-token.type';
-export * from 'src/view-models/auth/interfaces/access-token-payload.interface';
