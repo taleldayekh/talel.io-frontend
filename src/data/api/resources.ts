57f568affce0757c525c75c7ee70026151458271
taleldayekh <dev@talel.io>
2022-01-15 11:09:02 +0100

    Removing unused array of resources

--- src/data/api/resources.ts
+++ src/data/api/resources.ts
@@ -7,14 +7,9 @@ const ACCOUNTS_LOGIN = 'accounts/login';
 const ACCOUNTS_NEW_ACCESS_TOKEN = 'accounts/token';
 const ACCOUNTS_LOGOUT = 'accounts/logout';
 
-const resourcesWithAuthentication = [ACCOUNTS_LOGOUT];
-const resourcesWithoutAuthentication = [ACCOUNTS_LOGIN];
-
 export {
   API_V1_BASE_URL,
   ACCOUNTS_LOGIN,
   ACCOUNTS_NEW_ACCESS_TOKEN,
   ACCOUNTS_LOGOUT,
-  resourcesWithAuthentication,
-  resourcesWithoutAuthentication,
 };
