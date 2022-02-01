fd343fdf9563898908373bb3948ccf9251e0aa70
taleldayekh <dev@talel.io>
2022-02-01 22:48:44 +0100

    Adding GridView component and styles

---
+++ src/views/GridView/interfaces/grid-view-props.interface.ts
@@ -0,0 +1,10 @@
+import { ReactNode } from 'react';
+
+export interface GridViewProps {
+  children?: ReactNode;
+  className?: string;
+  container?: boolean;
+  s?: number;
+  m?: number;
+  l?: number;
+}
