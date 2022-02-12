4a26a423430471e3899910d527c857b9d9e971bd
taleldayekh <dev@talel.io>
2022-02-12 11:21:28 +0100

    Refactoring constructor

--- src/models/authentication/authentication.model.ts
+++ src/models/authentication/authentication.model.ts
@@ -1,11 +1,5 @@
 import TokenModel from 'src/models/authentication/token.model';
 
 export default class AuthenticationModel {
-  public token: TokenModel;
-  public isLoggedIn: boolean;
-
-  constructor(token: TokenModel, isLoggedIn: boolean) {
-    this.token = token;
-    this.isLoggedIn = isLoggedIn;
-  }
+  constructor(public token: TokenModel, public isLoggedIn: boolean) {}
 }
