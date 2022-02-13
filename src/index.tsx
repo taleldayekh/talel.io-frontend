f4093a9dba93bdb8bd9e6c43391cfe8a4584a673
taleldayekh <dev@talel.io>
2022-02-13 17:22:08 +0100

    Adding custom fonts to main CSS file

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from 'src/reportWebVitals';
 import { AuthenticationProvider } from 'src/contexts/authentication/authentication.context';
 import AppController from 'src/views/AppView/AppController';
+import 'src/theme/index.css';
 
 ReactDOM.render(
   <AuthenticationProvider>
