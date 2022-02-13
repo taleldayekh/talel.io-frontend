58d1775c9691210d31586a51cf4158072e24021c
taleldayekh <dev@talel.io>
2022-02-13 17:23:29 +0100

    Initial markup and styling for article content

--- src/views/ArticleView/ArticleView.tsx
+++ src/views/ArticleView/ArticleView.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import { ArticleViewProps } from 'src/views/ArticleView/interfaces';
+import styles from 'src/views/ArticleView/styles/styles.module.css';
 
 const ArticleView: React.FC<ArticleViewProps> = (props: ArticleViewProps) => {
   const { articleHTML } = props;
 
-  return <div dangerouslySetInnerHTML={{ __html: articleHTML }} />;
+  return (
+    <div className={styles.article}>
+      <div className={styles.article__meta}>
+        <p className={styles.article__meta__datetime}>February 13, 2022 </p>
+        <p className={styles.article__meta__author}>/ Words by Talel Dayekh</p>
+      </div>
+      <h1 className={styles.article__title}>Article Title</h1>
+      <div
+        className={styles.article__content}
+        dangerouslySetInnerHTML={{ __html: articleHTML }}
+      />
+    </div>
+  );
 };
 
 export default ArticleView;
