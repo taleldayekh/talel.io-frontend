a38f9d282cc2676db812176c5f47fc6cf40bb402
taleldayekh <dev@talel.io>
2022-02-13 17:24:05 +0100

    Mapping articles to ArticleViewModel

--- src/views/ArticleView/ArticleController.tsx
+++ src/views/ArticleView/ArticleController.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import DOMPurify from 'dompurify';
 import HttpClient from 'src/libs/http-client/http-client';
 import ArticlesRepository from 'src/data/articles/articles.repository';
+import ArticlesMapper from 'src/data/articles/articles.mapper';
+import ArticleModel from 'src/models/article/article.model';
 import ArticleViewModel from 'src/view-models/article/article.view-model';
 import ArticleView from 'src/views/ArticleView/ArticleView';
 
@@ -12,8 +14,11 @@ const ArticleController: React.FC = () => {
   const [articleHTML, setArticleHTML] = useState<string>();
 
   useEffect(() => {
-    articlesRepository.getAll().then((articles: ArticleViewModel[]) => {
-      const latestArticle = articles[0];
+    articlesRepository.getAll().then((articlesRes: ArticleModel[]) => {
+      const articles: ArticleViewModel[] = articlesRes.map(
+        (article: ArticleModel) => ArticlesMapper.toArticleViewModel(article),
+      );
+      const latestArticle = articles[4];
       const latestArticleHTML = DOMPurify.sanitize(latestArticle.html);
 
       setArticleHTML(latestArticleHTML);
