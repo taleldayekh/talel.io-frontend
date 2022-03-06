52fd9b87569957b49672c7a3bdfa683eabacd0f9
taleldayekh <dev@talel.io>
2022-03-06 20:04:37 +0100

    Adding routes for article views

--- src/views/AppView/AppView.tsx
+++ src/views/AppView/AppView.tsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import ProtectedViewsController from 'src/views/private/ProtectedViewsController/ProtectedViewsController';
+import { ArticlesProvider } from 'src/contexts/articles/articles.context';
+import HomeView from 'src/views/HomeView/HomeView';
+import ArticlesController from 'src/views/ArticlesView/ArticlesController';
+import ArticleController from 'src/views/ArticleView/ArticleController';
 import LoginController from 'src/views/LoginView/LoginController';
-import ArticlesView from 'src/views/private/ArticlesView/ArticlesView';
+import ProtectedViewsController from 'src/views/private/ProtectedViewsController/ProtectedViewsController';
+import ArticleFormController from 'src/views/private/ArticleFormView/ArticleFormController';
 
 const AppView: React.FC = () => {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<p>This is the landing page</p>} />
+        <Route path="/" element={<HomeView />} />
+        <Route
+          path="/articles/*"
+          element={
+            <ArticlesProvider>
+              <Routes>
+                <Route path="/" element={<ArticlesController />} />
+                <Route path=":slug" element={<ArticleController />} />
+              </Routes>
+            </ArticlesProvider>
+          }
+        />
         <Route path="/login" element={<LoginController />} />
         <Route
           path="/admin/*"
           element={
             <ProtectedViewsController>
               <Routes>
-                <Route path="articles" element={<ArticlesView />} />
+                <Route path="articles" element={<ArticleFormController />} />
               </Routes>
             </ProtectedViewsController>
           }
