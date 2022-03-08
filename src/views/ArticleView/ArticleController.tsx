a3f9d6e59c96cb61db819bf76de5c369f74404f4
taleldayekh <dev@talel.io>
2022-03-08 12:09:36 +0100

    Refactoring article controller

--- src/views/ArticleView/ArticleController.tsx
+++ src/views/ArticleView/ArticleController.tsx
@@ -1,52 +1,40 @@
-// TODO: Refactor to a ArticlesController for all articles
-import React, { useState, useEffect } from 'react';
-import { Article, ArticleMetadata } from 'src/views/ArticleView/interfaces';
-import DOMPurify from 'dompurify';
-import ArticleModel from 'src/models/article/article.model';
-import ArticleViewModel from 'src/view-models/article/article.view-model';
-import HttpClient from 'src/libs/http-client/http-client';
-import ArticlesRepository from 'src/data/articles/articles.repository';
+import React, { useContext, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { ArticlesContext } from 'src/contexts/articles/articles.context';
 import ArticlesMapper from 'src/data/articles/articles.mapper';
+import ArticleViewModel from 'src/view-models/article/article.view-model';
 import ArticleView from 'src/views/ArticleView/ArticleView';
 
 const ArticleController: React.FC = () => {
-  const [articlesRepository] = useState<ArticlesRepository>(
-    new ArticlesRepository(HttpClient),
-  );
+  const slug = useParams().slug;
 
-  const [article, setArticle] = useState<Article>();
-  const [metadata, setMetadata] = useState<ArticleMetadata>();
+  const { articles } = useContext(ArticlesContext);
+
+  const [article, setArticle] = useState<ArticleViewModel>();
 
   useEffect(() => {
-    articlesRepository
-      .getAll()
-      .then((articlesRes: ArticleModel[]) => {
-        const articles: ArticleViewModel[] = articlesRes.map(
-          (article: ArticleModel) => ArticlesMapper.toArticleViewModel(article),
-        );
+    if (!slug) return;
 
-        // TODO: Latest article should not be derived from this component
-        const [latestArticle] = articles.slice(-1);
+    const articleFromContext = articles.find(
+      (article) => article.slug === slug,
+    );
 
-        setArticle({
-          title: latestArticle.title,
-          content: DOMPurify.sanitize(latestArticle.html),
-        });
-        setMetadata({ createdAt: latestArticle.createdAt });
-      })
-      .catch((error) => {
-        // TODO: Proper error handling
-        console.log(error);
-      });
+    if (articleFromContext) {
+      setArticle(ArticlesMapper.toArticleViewModel(articleFromContext));
+    } else {
+      // ! Make db request with try catch
+      // ! Map to article view model and remove the as ArticleViewModel
+      const articleFromDb = {};
+
+      setArticle(articleFromDb as ArticleViewModel);
+    }
   }, []);
 
-  return article && metadata ? (
-    <ArticleView
-      title={article.title}
-      content={article.content}
-      creationDate={metadata.createdAt}
-    />
-  ) : null;
+  return article ? (
+    <ArticleView article={article} />
+  ) : (
+    <p>No article available</p>
+  );
 };
 
 export default ArticleController;
