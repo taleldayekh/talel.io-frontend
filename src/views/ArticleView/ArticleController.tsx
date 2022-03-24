bf72b81d1ef9b6463033118bf4cae4a8cdf97005
taleldayekh <dev@talel.io>
2022-03-24 08:55:14 +0100

    Retrieving article from db

--- src/views/ArticleView/ArticleController.tsx
+++ src/views/ArticleView/ArticleController.tsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import HttpClient from 'src/libs/http-client/http-client';
 import { ArticlesContext } from 'src/contexts/articles/articles.context';
+import ArticlesRepository from 'src/data/articles/articles.repository';
 import ArticlesMapper from 'src/data/articles/articles.mapper';
 import ArticleViewModel from 'src/view-models/article/article.view-model';
 import ArticleView from 'src/views/ArticleView/ArticleView';
@@ -9,6 +11,10 @@ const ArticleController: React.FC = () => {
   const slug = useParams().slug;
 
   const { articles } = useContext(ArticlesContext);
+
+  const [articlesRepository] = useState<ArticlesRepository>(
+    new ArticlesRepository(HttpClient),
+  );
 
   const [article, setArticle] = useState<ArticleViewModel>();
 
@@ -22,18 +28,20 @@ const ArticleController: React.FC = () => {
     if (articleFromContext) {
       setArticle(ArticlesMapper.toArticleViewModel(articleFromContext));
     } else {
-      // ! Make db request with try catch
-      // ! Map to article view model and remove the as ArticleViewModel
-      const articleFromDb = {};
-
-      setArticle(articleFromDb as ArticleViewModel);
+      // Todo: Error handling.
+      articlesRepository
+        .getBySlug(slug)
+        .then((article) =>
+          setArticle(ArticlesMapper.toArticleViewModel(article)),
+        );
     }
   }, []);
 
-  return article ? (
+  // Todo: Navigate to 404 if article cannot be found.
+  return article && article.title ? (
     <ArticleView article={article} />
   ) : (
-    <p>No article available</p>
+    <p>404 Not Found</p>
   );
 };
 
