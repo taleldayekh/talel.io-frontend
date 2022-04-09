bc1b02bf1aac6bda71f21d86d7006aae8c27b59f
taleldayekh <dev@talel.io>
2022-04-09 20:51:21 +0200

    Formatting dates in article view model

--- src/view-models/article/article.view-model.ts
+++ src/view-models/article/article.view-model.ts
@@ -1,12 +1,52 @@
 import DOMPurify from 'dompurify';
 
 export default class ArticleViewModel {
+  public createdDate = '';
+  public updatedDate = '';
+
   constructor(
     public title: string,
     public html: string,
-    public createdAt: string,
-    public updatedAt: string,
+    private createdAt: string,
+    private updatedAt: string,
   ) {
     this.html = DOMPurify.sanitize(html);
+    this.setFormattedDates();
+  }
+
+  setFormattedDates(): void {
+    const createdDate = new Date(this.createdAt);
+    const updatedDate = new Date(this.updatedAt);
+
+    const wasUpdated = updatedDate.getTime() > createdDate.getTime();
+
+    wasUpdated
+      ? (this.updatedDate = this.formatDate(updatedDate))
+      : (this.updatedDate = '');
+
+    this.createdDate = this.formatDate(createdDate);
+  }
+
+  formatDate(date: Date): string {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    return `${day} ${month} ${year}`;
   }
 }
