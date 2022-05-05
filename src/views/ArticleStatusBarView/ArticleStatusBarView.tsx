3824e27cc5e092ae058fcf086f2a4ed4c19fa891
taleldayekh <dev@talel.io>
2022-05-05 09:03:23 +0200

    Renaming article status bar styles file

--- src/views/ArticleStatusBarView/ArticleStatusBarView.tsx
+++ src/views/ArticleStatusBarView/ArticleStatusBarView.tsx
@@ -1,6 +1,6 @@
 import { ArticleStatusBarViewProps } from 'src/views/ArticleStatusBarView/interfaces';
 import ReadingPositionController from 'src/views/ReadingPositionView/ReadingPositionController';
-import styles from 'src/views/ArticleStatusBarView/styles/styles.module.css';
+import articleStatusBarStyles from 'src/views/ArticleStatusBarView/styles/articleStatusBar.styles.module.css';
 
 const ArticleStatusBarView = ({
   articleTitle,
@@ -10,13 +10,20 @@ const ArticleStatusBarView = ({
 }: ArticleStatusBarViewProps): JSX.Element => {
   return (
     <div
-      className={`${styles['article-status-bar']} ${
-        !articleTitleIsVisible && styles['article-status-bar--visible']
-      } ${footerIsVisible && styles['article-status-bar--hidden']}`}
+      className={`${articleStatusBarStyles['article-status-bar']} ${
+        !articleTitleIsVisible &&
+        articleStatusBarStyles['article-status-bar--visible']
+      } ${
+        footerIsVisible && articleStatusBarStyles['article-status-bar--hidden']
+      }`}
     >
       <ReadingPositionController contentRef={articleContentRef}>
-        <div className={styles['article-status-bar__content']}>
-          <p className={styles['article-status-bar__content__title']}>
+        <div className={articleStatusBarStyles['article-status-bar__content']}>
+          <p
+            className={
+              articleStatusBarStyles['article-status-bar__content__title']
+            }
+          >
             {articleTitle}
           </p>
         </div>
