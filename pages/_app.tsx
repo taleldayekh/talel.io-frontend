863f52a34b8855b348f987c62c9db77f4b2b67b6
taleldayekh <dev@talel.io>
2023-01-06 22:25:35 +0100

    Wrap root App component with AuthProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { AuthProvider } from 'contexts/auth/auth.context';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps}: AppProps) {
+  return (
+    <AuthProvider>
+      <Component { ...pageProps }/>
+    </AuthProvider>
+  )
 }
