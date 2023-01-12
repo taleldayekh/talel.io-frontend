0e042a8d79873a3f467a7a0c4fa4ec80914c6b4a
taleldayekh <dev@talel.io>
2023-01-12 21:28:07 +0100

    Register interceptor callback for returning token

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,30 @@
-import type { AppProps } from 'next/app';
-import { AuthProvider } from 'contexts/auth/auth.context';
+import { useContext, useEffect, ReactNode } from 'react';
+import { AppProps } from 'next/app';
+import { AuthProvider, AuthContext } from 'contexts/auth/auth.context';
+import httpClient from 'libs/http-client/http-client';
+import { RequestInterceptionEvents } from 'libs/http-client/enums';
+
+function AppController(component: ReactNode) {
+  const { authValues } = useContext(AuthContext);
+
+  useEffect(() => {
+    // Registers callback for returning access token
+    // from context during request interception.
+    httpClient.onRequestInterception(
+      RequestInterceptionEvents.TOKEN,
+      () => {
+        return authValues.token
+      }
+    )
+  }, [])
+
+  return component;
+}
 
 export default function App({ Component, pageProps}: AppProps) {
   return (
     <AuthProvider>
-      <Component { ...pageProps }/>
+      { AppController(<Component {...pageProps}/>) }
     </AuthProvider>
   )
 }
