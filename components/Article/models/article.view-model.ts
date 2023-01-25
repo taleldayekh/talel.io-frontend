187cf1aa5af2240184b8a64a52788627dfc48173
taleldayekh <dev@talel.io>
2023-01-25 08:45:56 +0100

    Create method for formatting dates

--- components/Article/models/article.view-model.ts
+++ components/Article/models/article.view-model.ts
@@ -9,10 +9,46 @@ export default class ArticleViewModel {
         private createdDate: Date,
         private updatedDate: Date,
     ) {
-        this.formatDates();
+        this.setDates();
     }
 
-    private formatDates(): void {
-        //
+    private formatDate(date: Date): string {
+        const months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ];
+
+        const day = date.getDate();
+        const month = months[date.getMonth()];
+        const year = date.getFullYear();
+        
+        return `${day} ${month} ${year}`;
+    }
+
+    private setDates(): void {
+        const articleWasUpdated = this.updatedDate.getTime() > this.createdDate.getTime();
+        const hoursSincePublish = Math.abs(Date.now() - this.createdDate.getTime()) / (1000*60*60);
+
+        if (hoursSincePublish < 24) {
+            this.createdAt = `${hoursSincePublish} ago`
+        } else {
+            this.createdAt = this.formatDate(this.createdDate);
+        }
+
+        if (articleWasUpdated) {
+            this.updatedAt = this.formatDate(this.updatedDate);
+        } else {
+            this.updatedAt = '';
+        }
     }
 }
