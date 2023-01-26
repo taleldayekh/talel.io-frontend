f076de50f25267d3331dd74ac03a369b16c6e068
taleldayekh <dev@talel.io>
2023-01-26 21:09:24 +0100

    Add logic for validating image type

--- utils/image/image-validators.ts
+++ utils/image/image-validators.ts
@@ -1,18 +1,29 @@
-// TODO: Add correct type
-export function isValidImageType(imageFile: any) {
-    const gifMagicNumberHeader = []
-    const pngMagicNumberHeader = []
-    const jpegMagicNumberHeader = []
+export const isValidImageType = (imageFile: File): Promise<boolean> => {
+    const gifMagicNumberHeader = new Uint8Array([0x47, 0x49, 0x46, 0x38])
+    const pngMagicNumberHeader = new Uint8Array([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a])
+    const jpegMagicNumberHeader = new Uint8Array([0xff, 0xd8, 0xff, 0xe0])
 
     return new Promise((resolve, reject) => {
         const fileReader = new FileReader();
 
         fileReader.onload = () => {
-            console.log(fileReader.result)
-            // Return Promise<boolean>
+            if (fileReader.result instanceof ArrayBuffer) {
+                const fileHeader = new Uint8Array(fileReader.result);
+
+                const isGif = gifMagicNumberHeader.every((header: number, index: number) => header === fileHeader[index]);
+                const isPng = pngMagicNumberHeader.every((header: number, index: number) => header === fileHeader[index]);
+                const isJpeg = jpegMagicNumberHeader.every((header: number, index: number) => header === fileHeader[index]);
+
+                resolve(isGif || isPng || isJpeg);
+                return;
+            }
+
+            resolve(false)
         }
 
-        // Reads the first 10 bytes
-        fileReader.readAsArrayBuffer(imageFile.slice(0, 10))
+        fileReader.onerror = reject;
+
+        // Reads the first 8 bytes
+        fileReader.readAsArrayBuffer(imageFile.slice(0, 8));
     })
 }
