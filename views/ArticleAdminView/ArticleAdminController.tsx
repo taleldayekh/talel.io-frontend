41e754bc1d33e2ddb56251072b54cfa512f963b2
taleldayekh <dev@talel.io>
2023-01-28 12:42:52 +0100

    Validate and upload image on drop

--- views/ArticleAdminView/ArticleAdminController.tsx
+++ views/ArticleAdminView/ArticleAdminController.tsx
@@ -1,16 +1,40 @@
 import { DragEvent } from 'react';
+import AssetsRepository from 'infrastructure/repositories/assets/assets.repository';
 import { ArticleAdminControllerProps } from 'views/ArticleAdminView/interfaces';
-import { isValidImageType } from 'utils/image/image-validators';
+import { isValidImageType, isValidImageSize } from 'utils/image/image-validators';
 
 export default function ArticleAdminController(props: ArticleAdminControllerProps) {
-    const uploadImageOnDrop = (event: DragEvent<HTMLTextAreaElement>) => {
+    const uploadImagesOnDrop = async (event: DragEvent<HTMLTextAreaElement>) => {
         event.preventDefault();
 
-        const files = event.dataTransfer.files;
+        const imageFiles = Array.from(event.dataTransfer.files);
+        let imageMarkdownString = '';
 
-        console.log(files);
-        isValidImageType(files[0])
+        for (const [index, imageFile] of imageFiles.entries()) {
+            try {
+                await isValidImageType(imageFile);
+                isValidImageSize(imageFile);
+
+                const imageFileName = imageFile.name;
+                const imageData = new FormData();
+
+                imageData.append(`${imageFileName}`, imageFile, `${imageFileName}`);
+
+                const uploadedImageRes = await AssetsRepository.uploadImage(imageData);
+                const imageSrc = uploadedImageRes.data.image_objects_urls[0];
+
+                const imageMarkdownSyntax = `![${imageFileName}](${imageSrc})`;
+
+                if (index === 0) {
+                    imageMarkdownString = imageMarkdownString + imageMarkdownSyntax;
+                } else {
+                    imageMarkdownString = imageMarkdownString + '\r\n' + '\r\n' + imageMarkdownSyntax;
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
     }
 
-    return props.render(uploadImageOnDrop);
+    return props.render(uploadImagesOnDrop);
 }
