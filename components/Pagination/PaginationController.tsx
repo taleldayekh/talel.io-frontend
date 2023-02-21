89e3b0432f1b19a08704ed486bd9241701654463
taleldayekh <dev@talel.io>
2023-02-21 21:41:19 +0100

    Fix remaining pagination controller issues

--- components/Pagination/PaginationController.tsx
+++ components/Pagination/PaginationController.tsx
@@ -1,19 +1,26 @@
 import { PaginationControllerProps } from 'components/Pagination/interfaces';
 
-export default function PaginationController({ count, page, onChange, defaultTruncationBoundary, firstPage, setVisiblePaginationRange, render }: PaginationControllerProps) {
+export default function PaginationController({ count, page, onChange, defaultTruncationBoundary, firstPage, setPaginationRange, render }: PaginationControllerProps) {
     const isPageWithinBoundaryFromStart = (page: number, defaultTruncationBoundary: number): boolean => {
-        // Between 1 - 4
+        // Active page is between 1 - 4
         return page > 0 && !(page > defaultTruncationBoundary - 1);
     }
 
     const isPageWithinBoundaryFromEnd = (page: number, count: number, defaultTruncationBoundary: number): boolean => {
-        // Between e.g. 6 - 9
+        // Active page is between e.g. 6 - 16 if page count is 20
         return page > (count - defaultTruncationBoundary + 1) && page <= count;
     }
 
+    const isPageOutsideBoundaries = (page: number, count: number, defaultTruncationBoundary: number): boolean => {
+        return page >= defaultTruncationBoundary && page <= (count - defaultTruncationBoundary + 2);
+    }
+
     const updateTruncatedPaginationRange = (activePage: number) => {
-        // 3 pages range
-        setVisiblePaginationRange({
+        /**
+         * Truncates with three visible page numbers
+         * when not within any truncation boundaries.
+         */
+        setPaginationRange({
             start: activePage - 2,
             end: activePage + 1,
         })
@@ -21,14 +28,14 @@ export default function PaginationController({ count, page, onChange, defaultTru
 
     const onPrevClicked = () => {
         const prevPage = page - 1;
-        const pageIsWithinBoundaryFromStart = isPageWithinBoundaryFromStart(page, defaultTruncationBoundary);
-        const pageIsWithinBoundaryFromEnd = isPageWithinBoundaryFromEnd(page, count, defaultTruncationBoundary)
 
         if (prevPage > 0) {
             onChange(prevPage)
         }
 
-        if (!pageIsWithinBoundaryFromStart && !pageIsWithinBoundaryFromEnd) {
+        const pageIsOutsideBoundaries = isPageOutsideBoundaries(prevPage, count, defaultTruncationBoundary);
+
+        if (pageIsOutsideBoundaries) {
             updateTruncatedPaginationRange(prevPage)
         }
     }
@@ -36,15 +43,13 @@ export default function PaginationController({ count, page, onChange, defaultTru
     const onNextClicked = () => {
         const nextPage = page + 1;
 
-        // ! Error
-        const pageIsWithinBoundaryFromStart = isPageWithinBoundaryFromStart(page, defaultTruncationBoundary);
-        const pageIsWithinBoundaryFromEnd = isPageWithinBoundaryFromEnd(page, count, defaultTruncationBoundary)
-
         if (nextPage <= count) {
             onChange(nextPage)
         }
 
-        if (!pageIsWithinBoundaryFromStart && !pageIsWithinBoundaryFromEnd) {
+        const pageIsOutsideBoundaries = isPageOutsideBoundaries(nextPage, count, defaultTruncationBoundary);
+
+        if (pageIsOutsideBoundaries) {
             updateTruncatedPaginationRange(nextPage)
         }
     }
@@ -59,20 +64,18 @@ export default function PaginationController({ count, page, onChange, defaultTru
             !pageIsWithinBoundaryFromStart &&
             !pageIsWithinBoundaryFromEnd
         ) {
-            // Update truncated pages
             updateTruncatedPaginationRange(page)
         }
 
-        // use firstPage variable
         if (pageIsWithinBoundaryFromStart || page === firstPage) {
-            setVisiblePaginationRange({
+            setPaginationRange({
                 start: firstPage,
                 end: defaultTruncationBoundary
             })
         }
-        // Maybe create some default end and first page variable
+
         if (pageIsWithinBoundaryFromEnd || page === count) {
-            setVisiblePaginationRange({
+            setPaginationRange({
                 start: count - defaultTruncationBoundary,
                 end: count - 1
             })
--- components/Pagination/interfaces/pagination-controller-props.interface.ts
+++ components/Pagination/interfaces/pagination-controller-props.interface.ts
@@ -4,12 +4,15 @@ import { PaginationRange } from 'components/Pagination/interfaces';
 export interface PaginationControllerProps {
     count: number;
     page: number;
-    paginationRange: PaginationRange;
-    setPaginationRange: Dispatch<SetStateAction<PaginationRange>>;
+    firstPage: number;
     onChange: (page: number) => void;
+    defaultTruncationBoundary: number;
+    setPaginationRange: Dispatch<SetStateAction<PaginationRange>>;
     render: (
         onPrevClicked: () => void,
         onNextClicked: () => void,
-        onPageClicked: (page: number) => void
+        onPageClicked: (page: number) => void,
+        isPageWithinBoundaryFromStart: (page: number, defaultTruncationBoundary: number) => boolean,
+        isPageWithinBoundaryFromEnd: (page: number, count: number, defaultTruncationBoundary: number) => boolean
     ) => ReactElement;
 }
