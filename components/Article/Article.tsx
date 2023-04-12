37b13f3756430b9d94a1433975c13eb2e1929bbc
Talel Dayekh <dev@talel.io>
2023-04-12 08:34:05 +0200

    Start styling article

--- components/Article/Article.tsx
+++ components/Article/Article.tsx
@@ -1,5 +1,38 @@
 import { ArticleProps } from 'components/Article/interfaces';
+import { streamster } from 'assets/fonts/fonts';
+import styles from 'components/Article/article.module.css';
 
-export default function Article({ article, articleContentRef, articleTitleRef }: ArticleProps) {
-    return <></>
+export default function Article({
+    article,
+    articleContentRef,
+    articleTitleRef
+}: ArticleProps) {
+    return (
+        <>
+            <main className={styles.article}>
+                <div className={styles.article__hero__gradient}/>
+                <div className={styles.article__hero__image}>
+                    <img src={article.featuredImage} alt={`${article.title} Hero Image`}/>
+                </div>
+                {/* <h1 className={`${streamster.className} ${styles.article__title}`}>
+                    {article.title}
+                </h1> */}
+                <div className={styles.article__content}>
+                    {/* <div className={styles['article__content__table-of-contents']}>
+                        <ul>
+                            <li>One</li>
+                            <li>Two</li>
+                            <li>Three</li>
+                            <li>Four</li>
+                            <li>Five</li>
+                        </ul>
+                    </div> */}
+                    <div
+                        className={styles.article__content__md}
+                        dangerouslySetInnerHTML={{ __html: article.html }}
+                    />
+                </div>
+            </main>
+        </>
+    )
 }
