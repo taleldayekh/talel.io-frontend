5878a7a5af474170e829d04995ace8e36de68345
taleldayekh <dev@talel.io>
2023-04-19 20:52:24 +0200

    Adapt article meta schema

--- infrastructure/repositories/articles/schemas/article-meta.schema.ts
+++ infrastructure/repositories/articles/schemas/article-meta.schema.ts
@@ -1,14 +1,12 @@
 export interface ArticleMetaSchema {
-    meta: {
-        adjacent_articles: {
-            next: {
-                title: string,
-                slug: string
-            } | null,
-            prev: {
-                title: string,
-                slug: string
-            } | null
-        }
-    },
+  adjacent_articles: {
+    next: {
+      title: string;
+      slug: string;
+    } | null;
+    prev: {
+      title: string;
+      slug: string;
+    } | null;
+  };
 }
