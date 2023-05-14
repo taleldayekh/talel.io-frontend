4643f0f745eb481ee96ed95eab3296c3c097c1eb
taleldayekh <dev@talel.io>
2023-05-14 18:16:15 +0200

    Set API URL in env variable

--- config.ts
+++ config.ts
@@ -1,7 +1,7 @@
-// TODO: Use env variables.
+const config = {
+  api: {
+    url: process.env.NEXT_PUBLIC_API_URL,
+  },
+};
 
-export default {
-    api: {
-        backendApiUrl: 'http://127.0.0.1:5000/api/v1'
-    }
-}
+export default config;
--- libs/http-client/http-client.ts
+++ libs/http-client/http-client.ts
@@ -1,63 +1,80 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosHeaders, AxiosResponse } from 'axios';
-import { HttpResponse } from 'libs/http-client/interfaces';
-import { RequestInterceptionEvents } from 'libs/http-client/enums';
+import axios, {
+  AxiosHeaders,
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+} from 'axios';
 import config from 'config';
+import { RequestInterceptionEvents } from 'libs/http-client/enums';
+import { HttpResponse } from 'libs/http-client/interfaces';
 
 class HttpClient {
-    private readonly axiosInstance: AxiosInstance;
-    private requestInterceptorCallbacks = new Map();
+  private readonly axiosInstance: AxiosInstance;
+  private requestInterceptorCallbacks = new Map();
 
-    constructor(baseURL: string) {
-        this.axiosInstance = axios.create({baseURL});
-        this.initializeRequestInterceptor();
-    }
+  constructor(baseURL: string) {
+    this.axiosInstance = axios.create({ baseURL });
+    this.initializeRequestInterceptor();
+  }
 
-    private initializeRequestInterceptor(): void {
-        this.axiosInstance.interceptors.request.use((config: AxiosRequestConfig) => {
-            const getToken = this.requestInterceptorCallbacks.get(RequestInterceptionEvents.TOKEN);
+  private initializeRequestInterceptor(): void {
+    this.axiosInstance.interceptors.request.use(
+      (config: AxiosRequestConfig) => {
+        const getToken = this.requestInterceptorCallbacks.get(
+          RequestInterceptionEvents.TOKEN,
+        );
 
-            if (getToken) {
-                const token = getToken();
+        if (getToken) {
+          const token = getToken();
 
-                if (token.length) {
-                    (config.headers as AxiosHeaders).set('Authorization', `Bearer ${token}`);
-                }
-            }
-
-            return config;
-        })
-    }
-
-    // TODO: Consider types for the callbacks
-    public onRequestInterception(event: RequestInterceptionEvents, callback: () => any): void {
-        this.requestInterceptorCallbacks.set(event, callback);
-    }
-
-    public async get(resource: string): Promise<AxiosResponse> {
-        try {
-            return await this.axiosInstance.get(resource);
-        } catch (error) {
-            // TODO: Add method for returning custom error object.
-            throw error;
+          if (token.length) {
+            (config.headers as AxiosHeaders).set(
+              'Authorization',
+              `Bearer ${token}`,
+            );
+          }
         }
-    }
 
-    // TODO: Replace any type.
-    public async post(resource: string, body?: any): Promise<HttpResponse<any>> {
-        try {
-            const postRes = await this.axiosInstance.post(resource, body, { withCredentials: true})
+        return config;
+      },
+    );
+  }
 
-            return {
-                status: postRes.status,
-                data: postRes.data
-            }
-        } catch (error) {
-            // TODO: Add method for returning custom error object.
-            throw error;
-        }
+  // TODO: Consider types for the callbacks
+  public onRequestInterception(
+    event: RequestInterceptionEvents,
+    callback: () => any,
+  ): void {
+    this.requestInterceptorCallbacks.set(event, callback);
+  }
+
+  public async get(resource: string): Promise<AxiosResponse> {
+    try {
+      return await this.axiosInstance.get(resource);
+    } catch (error) {
+      // TODO: Add method for returning custom error object.
+      throw error;
     }
+  }
+
+  // TODO: Replace any type.
+  public async post(resource: string, body?: any): Promise<HttpResponse<any>> {
+    try {
+      const postRes = await this.axiosInstance.post(resource, body, {
+        withCredentials: true,
+      });
+
+      return {
+        status: postRes.status,
+        data: postRes.data,
+      };
+    } catch (error) {
+      // TODO: Add method for returning custom error object.
+      throw error;
+    }
+  }
 }
 
-const baseURL = config.api.backendApiUrl;
+const baseURL = config.api.url as string;
 
 export default new HttpClient(baseURL);
