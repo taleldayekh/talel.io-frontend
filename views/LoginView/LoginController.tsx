836dcff2f44dfecd41eeb94691574ea86c72673f
taleldayekh <dev@talel.io>
2023-07-18 21:21:07 +0200

    Fix broken smooth scroll and style article images

--- views/LoginView/LoginController.tsx
+++ views/LoginView/LoginController.tsx
@@ -1,48 +1,48 @@
-import { useContext, useState, useEffect, ChangeEvent } from 'react';
-import { useRouter } from 'next/navigation';
 import { AuthContext } from 'contexts/auth/auth.context';
 import useAuth from 'hooks/auth/useAuth';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, useContext, useEffect, useState } from 'react';
 import { LoginControllerProps } from 'views/LoginView/interfaces';
 
 export default function LoginController(props: LoginControllerProps) {
-    const router = useRouter();
-    const auth = useAuth();
+  const router = useRouter();
+  const auth = useAuth();
 
-    const { authValues } = useContext(AuthContext);
-    const isLoggedIn = authValues.isLoggedIn;
+  const { authValues } = useContext(AuthContext);
+  const isLoggedIn = authValues.isLoggedIn;
 
-    useEffect(() => {
-        auth.refreshAccessToken().catch(() => {
-            console.warn('No current logged in user')
-        })
-    }, [])
+  useEffect(() => {
+    auth.refreshAccessToken().catch(() => {
+      console.warn('No current logged in user');
+    });
+  }, []);
 
-    useEffect(() => {
-        if (isLoggedIn) {
-            router.push('/admin');
-        }
-    }, [isLoggedIn])
-
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-
-    const updateEmailInput = (event: ChangeEvent<HTMLInputElement>): void => {
-        const email = event.target.value;
-        setEmail(email);
+  useEffect(() => {
+    if (isLoggedIn) {
+      router.push('/admin');
     }
+  }, [isLoggedIn]);
 
-    const updatePasswordInput = (event: ChangeEvent<HTMLInputElement>): void => {
-        const password = event.target.value;
-        setPassword(password);
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const updateEmailInput = (event: ChangeEvent<HTMLInputElement>): void => {
+    const email = event.target.value;
+    setEmail(email);
+  };
+
+  const updatePasswordInput = (event: ChangeEvent<HTMLInputElement>): void => {
+    const password = event.target.value;
+    setPassword(password);
+  };
+
+  const login = async (): Promise<void> => {
+    try {
+      await auth.login(email, password);
+    } catch (error) {
+      // TODO: Display error component
     }
+  };
 
-    const login = async (): Promise<void> => {
-        try {
-            await auth.login(email, password);
-        } catch (error) {
-            // TODO: Display error component
-        }
-    }
-
-    return props.render(updateEmailInput, updatePasswordInput, login);
+  return props.render(updateEmailInput, updatePasswordInput, login);
 }
