534c7b3f63bd57cd8c0ae06a8ba48a7b40dd5811
taleldayekh <dev@talel.io>
2023-10-16 21:19:02 +0200

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import config from 'config';
 import { AuthContext, AuthProvider } from 'contexts/auth/auth.context';
 import { RequestInterceptionEvents } from 'libs/http-client/enums';
 import httpClient from 'libs/http-client/http-client';
+import Script from 'next/script';
 import { PropsWithChildren, useContext, useEffect } from 'react';
 import 'styles/colors.css';
 import 'styles/global.css';
@@ -27,6 +29,26 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body>
+        {config.analytics.gaMeasurementId && (
+          <>
+            <Script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${config.analytics.gaMeasurementId}`}
+            />
+            <Script
+              id="google-analytics"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', '${config.analytics.gaMeasurementId}');
+                `,
+              }}
+            />
+          </>
+        )}
         <AuthProvider>
           <AppController>{children}</AppController>
         </AuthProvider>
--- contexts/auth/auth.context.tsx
+++ contexts/auth/auth.context.tsx
@@ -1,24 +1,22 @@
-import { createContext, PropsWithChildren, useState } from 'react';
 import { AuthContextValues, AuthValues } from 'contexts/auth/interfaces';
+import { createContext, PropsWithChildren, useState } from 'react';
 
 const initialAuthValues: AuthValues = {
-    token: '',
-    isLoggedIn: false,
-}
+  token: '',
+  isLoggedIn: false,
+};
 
-export const AuthContext = createContext<AuthContextValues>(
-    {
-        authValues: initialAuthValues,
-        setAuthValues: () => null,
-    }
-)
+export const AuthContext = createContext<AuthContextValues>({
+  authValues: initialAuthValues,
+  setAuthValues: () => null,
+});
 
-export const AuthProvider = ({children}: PropsWithChildren) => {
-    const [authValues, setAuthValues] = useState<AuthValues>(initialAuthValues)
+export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [authValues, setAuthValues] = useState<AuthValues>(initialAuthValues);
 
-    return (
-        <AuthContext.Provider value={{ authValues, setAuthValues }}>
-            { children }
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider value={{ authValues, setAuthValues }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
