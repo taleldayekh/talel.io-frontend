cc63d3442b4c5316581b8488309b8e0d0e020d1c
taleldayekh <dev@talel.io>
2023-10-26 19:31:06 +0200

    Redirect to latest article and fix routing warning

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -1,3 +1,15 @@
+'use client'
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 export default function Articles() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // TODO: Temporary redirect until articles page is implemented
+    router.push('/', { scroll: false });
+  });
+
   return <></>;
 }
