4d402ce50cd793c876d3d533eb1d5944fd1a7a2b
taleldayekh <dev@talel.io>
2024-04-24 18:23:25 +0200

    Replace buttons with SliderControls component

--- components/ImageSlider/ImageSlider.tsx
+++ components/ImageSlider/ImageSlider.tsx
@@ -4,16 +4,21 @@ import styles from 'components/ImageSlider/image-slider.module.css';
 import ImageSliderController from 'components/ImageSlider/ImageSliderController';
 import { Image, ImageSliderProps } from 'components/ImageSlider/interfaces';
 import { useEffect, useRef, useState } from 'react';
+import SliderControls from 'components/ImageSlider/components/SliderControls';
 
-export default function ImageSlider({ images }: ImageSliderProps) {
+export default function ImageSlider({ images, multiple }: ImageSliderProps) {
+  // States for managing images in the UI
   const [sliderImages, setSliderImages] = useState<Image[]>([]);
+  const [numberOfImagesInViewport, setNumberOfImagesInViewport] = useState<
+    number | undefined
+  >(multiple ? undefined : 1);
+  const [currentSlideIndex, setCurrentSlideIndex] = useState<number>(0);
 
-  const [numberOfImagesInViewport, setNumberOfImagesInViewport] =
-    useState<number>(2);
+  // States for managing slider interactions
   const [isTransitionEnabled, setIsTransitionEnabled] = useState<boolean>(true);
   const [isButtonEnabled, setIsButtonEnabled] = useState<boolean>(true);
 
-  // Style states
+  // States for inline CSS
   const [imageStyles, setImageStyles] = useState<Record<string, string>>({});
   const [transformStyles, setTransformStyles] = useState<
     Record<string, string>
@@ -23,64 +28,57 @@ export default function ImageSlider({ images }: ImageSliderProps) {
   });
 
   useEffect(() => {
-    const imagesCopy = [...images];
-    const remainderNumberOfImagesInViewport =
-      imagesCopy.length % numberOfImagesInViewport;
+    if (!numberOfImagesInViewport) return;
 
-    if (remainderNumberOfImagesInViewport > 0) {
-      /**
-       * Append copies of images from the start of the array to
-       * achieve a remainder of 0 and fill the viewport on each
-       * slider scroll.
-       */
-      const numberOfImagesToAppend =
-        numberOfImagesInViewport - remainderNumberOfImagesInViewport;
-      const imagesToAppend = imagesCopy.slice(0, numberOfImagesToAppend);
+    // TODO: Should be the else block
+    setSliderImages([...images]);
+  }, [numberOfImagesInViewport, images]);
 
-      imagesCopy.push(...imagesToAppend);
-      setSliderImages([...imagesCopy]);
-    } else {
-      setSliderImages([...imagesCopy]);
-    }
-  }, [images, numberOfImagesInViewport]);
-
-  // Style effect
   useEffect(() => {
-    const imageWidth = 100 / numberOfImagesInViewport;
+    if (!numberOfImagesInViewport) return;
 
-    const imageCarouselStyles = {
+    const imageWidth = multiple ? 100 / numberOfImagesInViewport : 100;
+
+    const styles = {
       flex: `0 0 ${imageWidth}%`,
       maxWidth: `${imageWidth}%`,
     };
 
-    setImageStyles(imageCarouselStyles);
-  }, [numberOfImagesInViewport]);
+    setImageStyles(styles);
+  }, [numberOfImagesInViewport, multiple]);
 
-  const elementRef = useRef<any>(null);
+  // TODO: Typing
+  const imagesWrapperRef = useRef<any>(null);
 
   return (
     <ImageSliderController
-      imagesWrapperRef={elementRef}
-      numberOfImagesInViewport={numberOfImagesInViewport}
+      imagesWrapperRef={imagesWrapperRef}
       sliderImages={sliderImages}
-      setNumberOfImagesInViewport={setNumberOfImagesInViewport}
       setSliderImages={setSliderImages}
+      numberOfImagesInViewport={numberOfImagesInViewport}
+      setNumberOfImagesInViewport={setNumberOfImagesInViewport}
       setIsTransitionEnabled={setIsTransitionEnabled}
       setIsButtonEnabled={setIsButtonEnabled}
       setTransformStyles={setTransformStyles}
       setPositionStyles={setPositionStyles}
-      render={(onNextClick, onPrevClick) => (
+      render={(onNextClick, onPrevClick, calculateNumberOfSlides) => (
         <>
-          <div className={styles['image-slider']}>
+          <div
+            className={`
+              ${styles['image-slider']} 
+              ${multiple ? styles['image-slider--multiple'] : null}
+            `}
+          >
             <div
-              ref={elementRef}
-              className={`${styles['image-slider__images-wrapper']}
-                 ${
-                   isTransitionEnabled
-                     ? styles['images-slider__images-wrapper--transition']
-                     : null
-                 }
-                `}
+              ref={imagesWrapperRef}
+              className={`
+                ${styles['image-slider__images-wrapper']} 
+                ${
+                  isTransitionEnabled
+                    ? styles['image-slider__images-wrapper--transition']
+                    : null
+                }
+              `}
               style={{ ...transformStyles, ...positionStyles }}
             >
               {sliderImages.map((image: Image, index: number) => (
@@ -92,167 +90,16 @@ export default function ImageSlider({ images }: ImageSliderProps) {
                 />
               ))}
             </div>
+            <SliderControls
+              onNextClick={onNextClick}
+              onPrevClick={onPrevClick}
+              numberOfSlides={calculateNumberOfSlides()}
+              currentSlideIndex={currentSlideIndex}
+              multiple={multiple}
+            />
           </div>
-          {/* TODO: Move buttons */}
-          <button onClick={isButtonEnabled ? onPrevClick : undefined}>
-            Prev
-          </button>
-          <button onClick={isButtonEnabled ? onNextClick : undefined}>
-            Next
-          </button>
         </>
       )}
     />
   );
-
-  // const [sliderImages, setSliderImages] = useState<Image[]>([]);
-  // const [currentSlide, setCurrentSlide] = useState<number>(0);
-  // const [isTransitionEnabled, setIsTransitionEnabled] = useState<boolean>(true);
-  // const [isButtonEnabled, setIsButtonEnabled] = useState<boolean>(true);
-  // const [transformStyles, setTransformStyles] = useState<
-  //   Record<string, string>
-  // >({
-  //   transform: 'translate(0)',
-  // });
-  // const [positionStyles, setPositionStyles] = useState<Record<string, string>>({
-  //   justifyContent: 'flex-start',
-  // });
-
-  // const elementRef = useRef<any>(null);
-
-  // useEffect(() => {
-  //   setSliderImages([...images]);
-  // }, [images]);
-
-  // return (
-  //   <ImageSliderController
-  //     sliderElementRef={elementRef}
-  //     sliderImages={sliderImages}
-  //     setSliderImages={setSliderImages}
-  //     setCurrentSlide={setCurrentSlide}
-  //     setIsTransitionEnabled={setIsTransitionEnabled}
-  //     setIsButtonEnabled={setIsButtonEnabled}
-  //     setPositionStyles={setPositionStyles}
-  //     setTransformStyles={setTransformStyles}
-  //     render={(
-  //       onNextClick,
-  //       onPrevClick,
-  //       updateSwipeStartValue,
-  //       updateSwipeEndValue,
-  //       onSwipeEnd,
-  //     ) => (
-  //       <div
-  //         className={styles['image-slider']}
-  //         onTouchStart={updateSwipeStartValue}
-  //         onTouchMove={updateSwipeEndValue}
-  //         onTouchEnd={isButtonEnabled ? onSwipeEnd : undefined}
-  //       >
-  //         <button className={styles.handle}></button>
-
-  //         <div
-  //           className={`${styles['image-slider__images']} ${
-  //             isTransitionEnabled
-  //               ? styles['image-slider__images--transition']
-  //               : null
-  //           }`}
-  //           // style={{ ...transformStyles, ...positionStyles }}
-  //           ref={elementRef}
-  //         >
-  //           {sliderImages.map((image: Image, index: number) => (
-  //             <img key={index} src={image.src} alt={image.alt} />
-  //           ))}
-  //         </div>
-
-  //         <button
-  //           className={styles.handle}
-  //           onClick={isButtonEnabled ? onNextClick : undefined}
-  //         ></button>
-  {
-    /* ! */
-  }
-  {
-    /* ! */
-  }
-  {
-    /* ! */
-  }
-  {
-    /* ! */
-  }
-  {
-    /* ! */
-  }
-  // <div className={styles['image-slider__interactions']}>
-  {
-    /* <div
-              className={styles['image-slider__interactions__slide-indicator']}
-            >
-              <div
-                className={
-                  styles[
-                    'image-slider__interactions__slide-indicator__shadow-wrapper'
-                  ]
-                }
-              >
-                {sliderImages.map((_, index: number) => (
-                  <div
-                    key={index}
-                    className={`${
-                      styles['image-slider__interactions__slide-indicator__dot']
-                    } 
-                       ${
-                         index === currentSlide
-                           ? styles[
-                               'image-slider__interactions__slide-indicator__pill'
-                             ]
-                           : null
-                       } 
-                       ${
-                         index === 0 && currentSlide !== index
-                           ? styles[
-                               'image-slider__interactions__slide-indicator__dot--small'
-                             ]
-                           : null
-                       } 
-                       ${
-                         index === sliderImages.length - 1 &&
-                         currentSlide !== index
-                           ? styles[
-                               'image-slider__interactions__slide-indicator__dot--small'
-                             ]
-                           : null
-                       }
-                      `}
-                  ></div>
-                ))}
-              </div>
-            </div> */
-  }
-  {
-    /* <button
-              onClick={isButtonEnabled ? onPrevClick : undefined}
-              aria-label="View previous slide"
-            >
-              <Arrow
-                className={styles['image-slider__interactions__button-icon']}
-                direction={'left'}
-                size={8}
-              />
-            </button>
-            <button
-              onClick={isButtonEnabled ? onNextClick : undefined}
-              aria-label="View next slide"
-            >
-              <Arrow
-                className={styles['image-slider__interactions__button-icon']}
-                direction={'right'}
-                size={8}
-              />
-            </button> */
-  }
-  //         </div>
-  //       </div>
-  //     )}
-  //   />
-  // );
 }
