06f22adee82d18202f85c3d612e6277d670cb352
taleldayekh <dev@talel.io>
2024-04-27 13:26:20 +0200

    Create dedicated component for slider controls

---
+++ components/ImageSlider/components/SliderControls.tsx
@@ -0,0 +1,99 @@
+import { Arrow } from 'components/Icons';
+import { SliderControlsProps } from 'components/ImageSlider/interfaces';
+import styles from 'components/ImageSlider/components/slider-controls.module.css';
+
+export default function SliderControls({
+  onNextClick,
+  onPrevClick,
+  numberOfSlides,
+  currentSlideIndex,
+  multiple,
+  isDisabled,
+}: SliderControlsProps) {
+  const pages = Array.from({ length: numberOfSlides });
+
+  if (multiple) {
+    return (
+      <>
+        <div className={styles['slider-controls-multiple']}>
+          <ul
+            className={styles['slider-controls-multiple__pagination-indicator']}
+          >
+            {pages.map((_, index: number) => (
+              <li
+                key={index}
+                className={`
+                  ${index === currentSlideIndex ? styles.active : null}
+                `}
+              ></li>
+            ))}
+          </ul>
+        </div>
+        <button
+          className={styles['slider-controls-multiple__button-prev']}
+          onClick={!isDisabled ? onPrevClick : undefined}
+        >
+          Prev
+        </button>
+        <button
+          className={styles['slider-controls-multiple__button-next']}
+          onClick={!isDisabled ? onNextClick : undefined}
+        >
+          Next
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <div className={styles['slider-controls-single']}>
+        <div className={styles['slider-controls-single__pagination-indicator']}>
+          <ul
+            className={
+              styles['slider-controls-single__pagination-indicator__shadow-bg']
+            }
+          >
+            {pages.map((_, index: number) => (
+              <li
+                key={index}
+                className={`
+                  ${
+                    styles['slider-controls-single__pagination-indicator__dot']
+                  } 
+                  ${
+                    (index === 0 && currentSlideIndex !== index) ||
+                    (index === pages.length - 1 && currentSlideIndex !== index)
+                      ? styles[
+                          'slider-controls-single__pagination-indicator__dot--small'
+                        ]
+                      : null
+                  }
+                  ${
+                    index === currentSlideIndex
+                      ? styles[
+                          'slider-controls-single__pagination-indicator__pill'
+                        ]
+                      : null
+                  } 
+                `}
+              ></li>
+            ))}
+          </ul>
+        </div>
+        <button onClick={!isDisabled ? onPrevClick : undefined}>
+          <Arrow
+            className={styles['slider-controls-single__button-icon']}
+            direction="left"
+            size={8}
+          />
+        </button>
+        <button onClick={!isDisabled ? onNextClick : undefined}>
+          <Arrow
+            className={styles['slider-controls-single__button-icon']}
+            direction="right"
+            size={8}
+          />
+        </button>
+      </div>
+    );
+  }
+}
