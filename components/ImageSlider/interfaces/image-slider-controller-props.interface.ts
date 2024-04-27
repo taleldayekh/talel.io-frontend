6933ca0bd6ce26865ebdbf91ff6ae6129512fbc9
taleldayekh <dev@talel.io>
2024-04-27 13:26:47 +0200

    Adjust interface

--- components/ImageSlider/interfaces/image-slider-controller-props.interface.ts
+++ components/ImageSlider/interfaces/image-slider-controller-props.interface.ts
@@ -1,29 +1,21 @@
 import { Image } from 'components/ImageSlider/interfaces';
-import {
-  Dispatch,
-  MutableRefObject,
-  ReactElement,
-  SetStateAction,
-} from 'react';
+import { MutableRefObject, Dispatch, SetStateAction, ReactElement } from 'react';
 
 export interface ImageSliderControllerProps {
-  imagesWrapperRef: MutableRefObject<any>;
-  numberOfImagesInViewport: number;
+  multiple: boolean | undefined;
+  imagesWrapperRef: MutableRefObject<HTMLDivElement | null>;
   sliderImages: Image[];
   setSliderImages: Dispatch<SetStateAction<Image[]>>;
-  setNumberOfImagesInViewport: Dispatch<SetStateAction<number>>;
+  numberOfImagesInViewport: number | undefined;
+  setNumberOfImagesInViewport: Dispatch<SetStateAction<number | undefined>>;
+  setCurrentSlideIndex: Dispatch<SetStateAction<number>>;
   setIsTransitionEnabled: Dispatch<SetStateAction<boolean>>;
-  setIsButtonEnabled: Dispatch<SetStateAction<boolean>>;
+  setIsControlsEnabled: Dispatch<SetStateAction<boolean>>;
   setTransformStyles: Dispatch<SetStateAction<Record<string, string>>>;
   setPositionStyles: Dispatch<SetStateAction<Record<string, string>>>;
-  // setCurrentSlide: Dispatch<SetStateAction<number>>;
   render: (
     onNextClick: () => void,
     onPrevClick: () => void,
-  ) => // onNextClick: () => void,
-  // onPrevClick: () => void,
-  // updateSwipeStartValue: (event: TouchEvent<HTMLDivElement>) => void,
-  // updateSwipeEndValue: (event: TouchEvent<HTMLDivElement>) => void,
-  // onSwipeEnd: () => void,
-  ReactElement;
+    calculateNumberOfSlides: () => number,
+  ) => ReactElement;
 }
