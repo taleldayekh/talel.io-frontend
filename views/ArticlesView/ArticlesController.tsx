bdec88c8d57baa85688a8f813e385a094c0d053b
taleldayekh <dev@talel.io>
2024-08-21 18:42:56 +0200

    Create temporary articles page

--- views/ArticlesView/ArticlesController.tsx
+++ views/ArticlesView/ArticlesController.tsx
@@ -5,22 +5,10 @@ export default function ArticlesController({
   render,
 }: ArticlesControllerProps) {
   const router = useRouter();
-  // get heroArticle [0]
-  // get latestArticles [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
-  // useEffect(() => {
-  //   ArticlesRepository.getArticles()
-  //     .then((articlesRes) => {
-  //       const articles = articlesRes.data;
-  //       const latestArticleSlug = articles.articles[0].slug;
-
-  //       router.push(`/articles/${latestArticleSlug}`, { scroll: false });
-  //     })
-  //     .catch((error) => {
-  //       // TODO: Error handling
-  //       console.log(error);
-  //     });
-  // });
-
-  return render();
+  // TODO: Consider a routing service
+  const navigateToArticle = (slug: string) => {
+    router.push(`/articles/${slug}`, { scroll: false });
+  };
+  return render(navigateToArticle);
 }
